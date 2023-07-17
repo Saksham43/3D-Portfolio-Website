a6bf8fd9f5fa6e8d5c8aff836c85c67a5aa84eeb
Saksham43 <137748200+Saksham43@users.noreply.github.com>
2023-07-17 17:26:33 +0530

    Add files via upload

---
+++ src/assets/index.js
@@ -0,0 +1,83 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import spotify from "./spotify.png"
+import portfolio from "./portfolio.png"
+import amazon from "./amazon.png"
+import band from "./band.jpg";
+
+import css from "./tech/css.png";
+import clogo from "./tech/clogo.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+import madhurima from "./madhurima.png"
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+import monkhood from "./company/monkhood.jpg";
+import enactus from "./company/enactus.png";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+import founder from "./founder.png";
+import instagram from "./instagram.png";
+
+
+export {
+  founder,
+  spotify,
+  logo,
+  backend,
+  portfolio,
+  amazon,
+  creator,
+  mobile,
+  madhurima,
+  web,
+  github,
+  menu,
+  enactus,
+  clogo,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  instagram,
+  threejs,
+  meta,
+  shopify,
+  band,
+  starbucks,
+  tesla,
+  monkhood,
+  carrent,
+  jobit,
+  tripguide,
+};
